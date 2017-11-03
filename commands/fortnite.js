a5e514af0d25d6e60ec8b8ad15a9cce651b8d74f
jeffroiscool <jeffroiscool@gmail.com>
2017-11-03 23:25:02 +0100

    Rewrite to use Fortnitestats API (Thanks Kohai!)

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -2,6 +2,8 @@ const Fetch = require('node-fetch')
 const Cheerio = require('cheerio')
 const Discord = require("discord.js")
 const Request = require("request-promise")
+const Moment = require("moment")
+require("moment-duration-format")
 
 exports.run = async (client, message, params = [], debug = false) => {
     if (params.length === 0) return
@@ -85,76 +87,78 @@ async function checkFortnitePlayerUpdated(username){
 
 
 async function getFortnitePlayerStats(username){
-    let url = `https://fortnitestats.net/stats/${username}`
-    let res = await Fetch(url)
-    let data = await res.text();
-    if(res.ok){
-        let $ = Cheerio.load(data)
-        let totalKills = $(".panel-green").find("h3").text()
-        let soloKills = $("h4:contains('Kills')").eq(0).prev().text()
-        let duoKills = $("h4:contains('Kills')").eq(1).prev().text()
-        let squadKills = $("h4:contains('Kills')").eq(2).prev().text()
+    let url = `https://fortnitestats.net/api.php?player=${username}`
+    let data = await Fetch(url)
+    let json = await data.json()
 
-        let overallKD = $(".panel-orange").find("h3").text()
-        let soloKD = $("h4:contains('K/D Ratio')").eq(0).prev().text()
-        let duoKD = $("h4:contains('K/D Ratio')").eq(1).prev().text()
-        let squadKD = $("h4:contains('K/D Ratio')").eq(2).prev().text()
-        
-        let totalWins = $(".panel-red").find("h3").text()
-        let soloWins = $("h4:contains('Wins')").eq(0).prev().text()
-        let duoWins = $("h4:contains('Wins')").eq(1).prev().text()
-        let squadWins = $("h4:contains('Wins')").eq(2).prev().text()
+    let soloKills = json.solo_kills
+    let duoKills = json.duo_kills
+    let squadKills = json.squad_kills
+    let totalKills = soloKills + duoKills + squadKills
 
-        let totalMatches = $(".panel-purple").find("h3").text()
-        let soloMatches = $("h3:contains('solo Stats')").eq(0).next().text().replace(/[^0-9]/g, '')
-        let duoMatches = $("h3:contains('duo Stats')").eq(0).next().text().replace(/[^0-9]/g, '')
-        let squadMatches = $("h3:contains('Squad Stats')").eq(0).next().text().replace(/[^0-9]/g, '')
-        
-        let totalLoss = totalMatches - totalWins
-        let soloLoss = soloMatches - soloWins
-        let duoLoss = duoMatches - duoWins
-        let squadLoss = squadMatches - squadWins
+    let soloWins = json.solo_wins
+    let duoWins = json.duo_wins
+    let squadWins = json.squad_wins
+    let totalWins = soloWins + duoWins + squadWins
 
-        let hoursPlayed = $(".panel-pink").find("h3").text()
+    let soloMatches = json.solo_matches
+    let duoMatches = json.duo_matches
+    let squadMatches = json.squad_matches
+    let totalMatches = soloMatches + duoMatches + squadMatches
+    
+    let soloKD = parseFloat(json.solo_kd).toFixed(2)
+    let duoKD = parseFloat(json.duo_kd).toFixed(2)
+    let squadKD = parseFloat(json.squad_kd).toFixed(2)
+    let overallKD = (totalKills / totalMatches).toFixed(2)
 
-        let overallWinrate = Math.floor(parseInt($(".panel-blue").find("h3").text())) + "%"
-        let soloWinrate = Math.floor(soloWins / soloMatches * 100) + "%"
-        let duoWinrate = Math.floor(duoWins / duoMatches * 100) + "%"
-        let squadWinrate = Math.floor(squadWins / squadMatches * 100) + "%"
-        
-        let solo5 = $("h4:contains('Top 5')").eq(0).prev().text()
-        let duo5 = $("h4:contains('Top 5')").eq(1).prev().text()
-        let squad5 = $("h4:contains('Top 5')").eq(2).prev().text()
-        let total5 = parseInt(solo5) + parseInt(duo5) + parseInt(squad5)
+    let totalLoss = totalMatches - totalWins
+    let soloLoss = soloMatches - soloWins
+    let duoLoss = duoMatches - duoWins
+    let squadLoss = squadMatches - squadWins
+    
+    let hoursSolo = Moment.duration(json.solo_time, "minutes").format("d [day(s)] h [hrs] m [min]")
+    let hoursDuo = Moment.duration(json.duo_time, "minutes").format("d [day(s)] h [hrs] m [min]")
+    let hoursSquad = Moment.duration(json.squad_time, "minutes").format("d [day(s)] h [hrs] m [min]")
+    let hoursPlayed = Moment.duration(+(parseInt(json.solo_time) + parseInt(json.duo_time) + parseInt(json.squad_time)).toFixed(2), "minutes").format("d [day(s)] h [hrs] m [min]")
 
-        let solo10 = $("h4:contains('Top 10')").eq(0).prev().text()
-        let duo10 = $("h4:contains('Top 10')").eq(1).prev().text()
-        let squad10 = $("h4:contains('Top 10')").eq(2).prev().text()
-        let total10 = parseInt(solo10) + parseInt(duo10) + parseInt(squad10)
-        
-        let solo25 = $("h4:contains('Top 25')").eq(0).prev().text()
-        let duo25 = $("h4:contains('Top 25')").eq(1).prev().text()
-        let squad25 = $("h4:contains('Top 25')").eq(2).prev().text()
-        let total25 = parseInt(solo25) + parseInt(duo25) + parseInt(squad25)
+    let overallWinrate = Math.floor(totalWins / totalMatches * 100) + "%"
+    /*
+    let soloWinrate = Math.floor(soloWins / soloMatches * 100) + "%"
+    let duoWinrate = Math.floor(duoWins / duoMatches * 100) + "%"
+    let squadWinrate = Math.floor(squadWins / squadMatches * 100) + "%"
+    */
+    
+    let solo5 = json.solo_top5
+    let duo5 = json.duo_top5
+    let squad5 = json.squad_top5
+    let total5 = parseInt(solo5) + parseInt(duo5) + parseInt(squad5)
 
-        let lastUpdated = $(".user-info").find("p").text()
-        let embed = new Discord.RichEmbed()
-            .setURL(url)
-            .setDescription(`[Profile](${url})\n\n${totalMatches} total matches played (${overallWinrate} won) over ${Math.floor(hoursPlayed)} hours.`)
-            .setAuthor(username, "https://static.blog.playstation.com/wp-content/uploads/avatars/ccP8lTm_400x400.jpeg")
-            .setColor("#FF0000")
-            .setFooter(lastUpdated)
-            .addField("Kills", `Total: ${totalKills}\nSolo: ${soloKills}\nDuos: ${duoKills}\nSquads: ${squadKills}`, true)
-            .addField("K/D Ratio", `Total: ${overallKD}\nSolo: ${soloKD}\nDuos: ${duoKD}\nSquads: ${squadKD}`, true)
-            .addField("Top 5's", `Total: ${total5}\nSolo: ${solo5}\nDuos: ${duo5}\nSquads: ${squad5}`, true)
-            .addField("Matches", `Total: ${totalMatches}\nSolo: ${soloMatches}\nDuos: ${duoMatches}\nSquads: ${squadMatches}`, true)
-            .addField("Wins", `Total: ${totalWins}\nSolo: ${soloWins}\nDuos: ${duoWins}\nSquads: ${squadWins}`, true)
-            .addField("Top 10's", `Total: ${total10}\nSolo: ${solo10}\nDuos: ${duo10}\nSquads: ${squad10}`, true)
-            .addField("Losses", `Total: ${totalLoss}\nSolo: ${soloLoss}\nDuos: ${duoLoss}\nSquads: ${squadLoss}`, true)
-            .addField("Overall Winrate", `Total: ${overallWinrate}\nSolo: ${soloWinrate}\nDuos: ${duoWinrate}\nSquads: ${squadWinrate}`, true)
-            .addField("Top 25's", `Total: ${total25}\nSolo: ${solo25}\nDuos: ${duo25}\nSquads: ${squad25}`, true)
+    let solo10 = json.solo_top10
+    let duo10 = json.duo_top10
+    let squad10 = json.squad_top10
+    let total10 = parseInt(solo10) + parseInt(duo10) + parseInt(squad10)
+    
+    let solo25 = json.solo_top25
+    let duo25 = json.duo_top25
+    let squad25 = json.squad_top25
+    let total25 = parseInt(solo25) + parseInt(duo25) + parseInt(squad25)
 
-        return embed
-    }
-    return null
+    let lastUpdated = json.last_updated
+    let embed = new Discord.RichEmbed()
+        .setURL(`https://fortnitestats.net/stats/${username}`)
+        .setDescription(`[Profile](https://fortnitestats.net/stats/${username})\n\n${totalMatches} total matches played (${overallWinrate} won) over ${(hoursPlayed)}.`)
+        .setAuthor(username, "https://static.blog.playstation.com/wp-content/uploads/avatars/ccP8lTm_400x400.jpeg")
+        .setColor("#FF0000")
+        .setFooter(lastUpdated)
+        .addField("Kills", `Total: ${totalKills}\nSolo: ${soloKills}\nDuos: ${duoKills}\nSquads: ${squadKills}`, true)
+        .addField("K/D Ratio", `Total: ${overallKD}\nSolo: ${soloKD}\nDuos: ${duoKD}\nSquads: ${squadKD}`, true)
+        .addField("Top 5's", `Total: ${total5}\nSolo: ${solo5}\nDuos: ${duo5}\nSquads: ${squad5}`, true)
+        .addField("Matches", `Total: ${totalMatches}\nSolo: ${soloMatches}\nDuos: ${duoMatches}\nSquads: ${squadMatches}`, true)
+        .addField("Wins", `Total: ${totalWins}\nSolo: ${soloWins}\nDuos: ${duoWins}\nSquads: ${squadWins}`, true)
+        .addField("Top 10's", `Total: ${total10}\nSolo: ${solo10}\nDuos: ${duo10}\nSquads: ${squad10}`, true)
+        .addField("Losses", `Total: ${totalLoss}\nSolo: ${soloLoss}\nDuos: ${duoLoss}\nSquads: ${squadLoss}`, true)
+        .addField("Hours played", `Total: ${hoursPlayed}\nSolo: ${hoursSolo}\nDuos: ${hoursDuo}\nSquads: ${hoursSquad}`, true)
+        .addField("Top 25's", `Total: ${total25}\nSolo: ${solo25}\nDuos: ${duo25}\nSquads: ${squad25}`, true)
+
+    return embed
 }
